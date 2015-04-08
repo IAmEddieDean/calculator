8974a5b03b1144b6544e57da5b652d2e07a4a956
Chris Oyler <christopher.oyler@gmail.com>
2015-04-07 23:06:18 -0700

    layout done, logic started

--- client/index.js
+++ client/index.js
@@ -1,2 +1,20 @@
 'use strict';
-console.log('apollo -> online');
+
+$(document).ready(init);
+
+function init(){
+  var checkLength = funtion(number) {
+    if (number.length > 9) {
+      totaldiv.text(number.substr(number.length-9,9));
+      if (number.length > 15) {
+        number = '';
+        totaldiv.text("I'm afraid I can't do that Dave");
+      }
+    }
+  };
+  var number = '';
+  var newNumber = '';
+  var operator = '';
+  var totaldiv = $('#total');
+
+}
