f35855cdfead10681a46702fe92c49885683115d
Chris Oyler <christopher.oyler@gmail.com>
2015-04-08 01:25:06 -0700

    most buttons work

--- client/index.js
+++ client/index.js
@@ -3,18 +3,62 @@
 $(document).ready(init);
 
 function init(){
-  var checkLength = funtion(number) {
+  var checkLength = function(number) {
     if (number.length > 9) {
-      totaldiv.text(number.substr(number.length-9,9));
+      result.text(number.substr(number.length-9,9));
       if (number.length > 15) {
         number = '';
-        totaldiv.text("I'm afraid I can't do that Dave");
+        result.text("I'm afraid I can't do that Dave");
       }
     }
   };
   var number = '';
   var newNumber = '';
   var operator = '';
-  var totaldiv = $('#total');
-
+  var result = $('#total');
+result.text('0');
+$('.number').click(function(){
+  number += $(this).text();
+  result.text(number);
+  checkLength(number);
+});
+$('.operator').click(function(){
+  operator = $(this).text();
+  newNumber = number;
+  number = '';
+  result.text('');
+});
+  $('.operatorR').click(function(){
+    operator = $(this).text();
+    newNumber = number;
+    number = '';
+    result.text('');
+});
+$('#clear').click(function(){
+  number = '';
+  result.text('');
+    newNumber = '';
+});
+$('#equal').click(function(){
+  switch(operator) {
+    case '+':
+      result = (parseFloat(number) + parseFloat(newNumber)).toString(10);
+      break;
+    case '-':
+      result = (parseFloat(number) - parseFloat(newNumber)).toString(10);
+      break;
+    case '*':
+      result = (parseFloat(number) * parseFloat(newNumber)).toString(10);
+      break;
+    case '/':
+      result = (parseFloat(number) / parseFloat(newNumber)).toString(10);
+      break;
+    //case '%':
+      //result = (parseFloat(number) / 100).toString(10);
+  }
+  result.text(number);
+  checkLength(number);
+  number = '';
+  newNumber = '';
+});
 }
