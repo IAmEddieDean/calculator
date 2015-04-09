85ed83e974554221647987bf817a37cb19bd0237
Chris Oyler <christopher.oyler@gmail.com>
2015-04-08 22:27:28 -0700

    equal works properly

--- client/index.js
+++ client/index.js
@@ -2,76 +2,102 @@
 
 $(document).ready(init);
 
+var storedNumber;
+var storedNumber2;
+var operator = '';
+
 function init(){
-  var checkLength = function(number) {
-    if (number.length > 9) {
-      result.text(number.substr(number.length-9,9));
-      if (number.length > 15) {
-        number = '';
-        result.text("I'm afraid I can't do that Dave");
-      }
-    }
-  };
-  var number = '';
-  var newNumber = '';
-  var operator = '';
-  var result = $('#total');
-result.text('0');
-$('.number').click(function(){
-  number += $(this).text();
-  result.text(number);
-  checkLength(number);
-});
-$('.operator').click(function(){
-  operator = $(this).text();
-  newNumber = number;
-  number = '';
-  result.text(newNumber);
-});
-/*$('#dec').click(function(){
-    for(var i = 0; i < number.length; i++){
-      if (number[i] = '.') {
-        number = number;
-      }
-      else {
-        number = number + $(this).text();
-    }
+  $('.number').click(clickNumber);
+  $('#decimal').click(clickDecimal);
+  $('#clear').click(clear);
+  $('#perc').click(clickPercent);
+  $('#posNeg').click(clickNeg);
+  $('.operator').click(clickOperator);
+  $('#equal').click(performCalculation);
+}
+
+function clickNumber(){
+  var num = $(this).text();
+  var display = $('#display').text();
+  var output = (display === '0') ? num : display + num;
+  $('#display').text(output);
+}
+
+function clickDecimal(){
+  var display = $('#display').text();
+  var output = display.indexOf('.') !== -1 ? display : display += '.';
+  $('#display').text(output);
+}
+
+function clear(){
+  if ($('#display').text()==='0'){
+    storedNumber = undefined;
+    storedNumber2 = undefined;
+    $('#clear').text('C');
+  }else {
+    storedNumber = parseFloat($('#display').text());
+    $('#clear').text('AC');
   }
-  result.text(number);
-})*/
-$('#clear').click(function(){
-  number = '';
-  result.text('');
-  newNumber = '';
-});
-$('#posNeg').click(function(){
-  number = parseFloat(number * -1);
-  result.text(number);
-});
-$('#perc').click(function(){
-  number = parseFloat(number / 100);
-  result.text(number);
-});
-$('#equal').click(function(){
-  //number = parseFloat(number * 1);
-  //newNumber = parseFloat(newNumber * 1);
-  switch(operator) {
+  $('#display').text('0');
+}
+
+function clickPercent(){
+  var display = parseFloat($('#display').text());
+  var output = display / 100;
+  $('#display').text(output);
+}
+function clickNeg(){
+  var display = -1 * (parseFloat($('#display').text()));
+  $('#display').text(display);
+}
+
+function clickOperator(){
+  /*if (storedNumber2 !== undefined){
+    performCalculation();
+  }
+  else {
+    storedNumber = parseFloat($('#display').text());
+    $('#display').text('0');
+  }*/
+  storedNumber = parseFloat($('#display').text());
+  operator = $(this).text();
+  $('#display').text('0');
+}
+
+function performCalculation() {
+  var result;
+  if(storedNumber2 !== undefined){
+  switch (operator) {
     case '+':
-      number = (parseFloat((number * 1) + (newNumber * 1))).toString(10);
+      result = storedNumber + storedNumber2;
       break;
     case '-':
-      number = (parseFloat((newNumber * 1) - (number * 1))).toString(10);
+      result = storedNumber2 - storedNumber;
       break;
-    case '*':
-      number = (parseFloat((number * 1) * (newNumber * 1))).toString(10);
+    case '×':
+      result = storedNumber * storedNumber2;
       break;
-    case '/':
-      number = (parseFloat((newNumber * 1) / (number * 1))).toString(10);
+    case '÷':
+      result = storedNumber2 / storedNumber;
   }
-  //debugger;
-  result.text(number);
-  checkLength(number);
-  //number = '';
-  newNumber = '';
-});
+}
+  else{
+  var newNumber = parseFloat($('#display').text());
+  switch (operator) {
+    case '+':
+      result = storedNumber + newNumber;
+      break;
+    case '-':
+      result = storedNumber - newNumber;
+      break;
+    case '×':
+      result = storedNumber * newNumber;
+      break;
+    case '÷':
+      result = storedNumber / newNumber;
+    }
+    storedNumber = newNumber;
+  }
+  storedNumber2 = result;
+  $('#display').text(+result.toFixed(7));
 }
